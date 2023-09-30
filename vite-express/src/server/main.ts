0858fa7fb2d8de386e17d02dfea914991a61c117
Ko1ors <kolorsnik181200@gmail.com>
2023-09-30 17:54:42 +0300

    Express: some swagger setup, get nfts count and mint a NFT endpoints

--- vite-express/src/server/main.ts
+++ vite-express/src/server/main.ts
@@ -3,8 +3,47 @@ import "express-async-errors";
 import ViteExpress from "vite-express";
 import apiRouter from "./routes/api";
 import nftRouter from "./routes/nft";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 
 const app = express();
+
+const options = {
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "Express API",
+      version: "0.1.0",
+      description: "",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "",
+        url: "",
+        email: "",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./routes/*.ts"],
+};
+
+const specs = swaggerJsdoc(options);
+
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs, { explorer: true })
+);
+
+app.use(express.json());
+
 
 app.use('/api', apiRouter);
 app.use('/api/nft', nftRouter);
--- vite-express/src/server/routes/nft.ts
+++ vite-express/src/server/routes/nft.ts
@@ -1,14 +1,103 @@
 import express from 'express';
 import getContractAsync from '../shared/getContractAsync';
 import 'dotenv/config'
+import { Request, Response, Router } from 'express';
+import getPrivateSdk from '../shared/getPrivateSdk';
+
+const elements = ["Chronos", "Gaia", "Aether"] as const;
+export type Element = typeof elements[number];
+
+const classes = ["Melee", "Ranged"] as const;
+export type CardClass = typeof classes[number];
+
+const rarity = ["Common", "Rare", "Epic", "Legendary"] as const;
+export type Rarity = typeof rarity[number];
+
+const validateElement = (element: any): element is Element => {
+    return elements.includes(element);
+}
+
+const validateClass = (classType: any): classType is CardClass => {
+    return classes.includes(classType);
+}
+
+const validateRarity = (rarityType: any): rarityType is Rarity => {
+    return rarity.includes(rarityType);
+}
+
+interface NFTMetadata {
+  name: string;
+  description: string;
+  image: string;
+  element: Element;
+  class: CardClass;
+  power: number;
+  health: number;
+  agility: number;
+  rarity: Rarity;
+}
 
 const router = express.Router();
 
-// TODO: not secure. Only for demo purposes
-router.get('/', async function(req, res, next) {
+// Get all NFTs
+router.get('/', async (req, res) => {
     const contract = await getContractAsync();
     const nfts = await contract.erc1155.getAll();
     res.send(nfts);
 });
+
+// Get NFTs count
+router.get('/count', async (req, res) => {
+    const contract = await getContractAsync();
+    const count = await contract.erc1155.totalCount();
+    res.send(count);
+});
+
+// Mint a new NFT
+router.post('/mint', async (req: Request<NFTMetadata>, res) => {
+    const metadata = req.body as NFTMetadata;
+
+    if(!metadata) 
+        return res.status(400).send('Metadata is required');
+    if(!metadata.name)
+        return res.status(400).send('Name is required');
+    if(!metadata.description)
+        return res.status(400).send('Description is required');
+    if(!metadata.image)
+        return res.status(400).send('Image is required');
+    if(!validateElement(metadata.element))
+        return res.status(400).send('Element is required');
+    if(!validateClass(metadata.class))
+        return res.status(400).send('Class is required');
+    if(!metadata.power)
+        return res.status(400).send('Power is required');
+    if(!metadata.health)
+        return res.status(400).send('Health is required');
+    if(!metadata.agility)
+        return res.status(400).send('Agility is required');
+    if(!validateRarity(metadata.rarity))
+        return res.status(400).send('Rarity is required');
+
+    // Custom metadata of the NFTs to create
+    const metadatas = [metadata];
+    // Example:
+    // const metadatas = [{
+    //     name: "Cool NFT",
+    //     description: "This is a cool NFT",
+    //     image: fs.readFileSync("path/to/image.png"), // This can be an image url or file
+    // }, {
+    //     name: "Cool NFT",
+    //     description: "This is a cool NFT",
+    //     image: fs.readFileSync("path/to/image.png"),
+    // }];
+
+    // Use private sdk to sign the transaction
+    const sdk = getPrivateSdk();
+    const contract = await getContractAsync(sdk);
+    const results = await contract.erc1155.lazyMint(metadatas); // uploads and creates the NFTs on chain
+    res.send(results);
+});
+
+
 
 export default router;
