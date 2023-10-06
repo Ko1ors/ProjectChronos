f553312d613638a5641929e8cb4e7868b1e7146d
Ko1ors <kolorsnik181200@gmail.com>
2023-10-06 16:57:52 +0300

    Express: endpoints to create packs

--- vite-express/src/server/routes/packs.ts
+++ vite-express/src/server/routes/packs.ts
@@ -14,4 +14,97 @@ router.get('/', async (req, res) => {
     res.send(nfts);
 });
 
+interface PackReward {
+    contractAddress: string;
+    tokenId: number;
+    quantityPerReward: number;
+    totalRewards: number;
+}
+
+const validatePackRewards = (rewards: any): rewards is PackReward[] => {
+    return rewards && rewards.length > 0 && rewards.every((reward: any) => {
+        return reward.contractAddress && reward.quantityPerReward && (reward.tokenId || reward.tokenId == 0) && reward.totalRewards;
+    });
+}
+
+const validatePackCreation = (req: Request, res: Response) => {
+    const { name, description, image, rewards, rewardsPerPack } = req.body;
+
+    if (!name)
+        return  {success: false, result: "Name is required"}
+    if (!rewards)
+        return {success: false, result: "Rewards is required"}
+    if (!validatePackRewards(rewards))
+        return {success: false, result: "Rewards is invalid"}
+
+    // We only use ERC1155 rewards right now
+    const pack = {
+        // The metadata for the pack NFT itself
+        packMetadata: {
+            name: name,
+            description: description,
+            image: image,
+        },
+        // ERC20 rewards to be included in the pack
+        // erc20Rewards: [
+        //     {
+        //         assetContract: "0x...",
+        //         quantityPerReward: 5,
+        //         quantity: 100,
+        //         totalRewards: 20,
+        //     }
+        // ],
+        // // ERC721 rewards to be included in the pack
+        // erc721Rewards: [
+        //     {
+        //         assetContract: "0x...",
+        //         tokenId: 0,
+        //     }
+        // ],
+        // ERC1155 rewards to be included in the pack
+        erc1155Rewards: rewards,
+        openStartTime: new Date(), // the date that packs can start to be opened, defaults to now
+        rewardsPerPack: rewardsPerPack ?? 1, // the number of rewards in each pack, defaults to 1
+    }
+    return {success: true, result: pack};
+}
+
+// Create a new Pack
+router.post('/create', async (req, res) => {
+    const validationResult = validatePackCreation(req, res);
+    if(!validationResult.success)
+        return res.status(400).send(validationResult.result);
+
+    const sdk = getPrivateSdk();
+    const contract = await getPacksContractAsync(sdk);
+    const transaction = await contract.create(validationResult.result as any);
+    res.send(transaction);
+});
+
+// Create a new Pack to wallet
+router.post('/createToWallet', async (req, res) => {
+        // Address of the wallet to create the pack to
+    const { address } = req.body;
+    if (!address)
+        return res.status(400).send("Address is required");
+
+    const validationResult = validatePackCreation(req, res);
+    if(!validationResult.success)
+        return res.status(400).send(validationResult.result);
+
+    const sdk = getPrivateSdk();
+    const contract = await getPacksContractAsync(sdk);
+    const transaction = await contract.createTo(address, validationResult.result as any);
+    res.send(transaction);
+});
+
+// Set approval for a pack
+router.post('/setApproval', async (req, res) => {
+
+    const sdk = getPrivateSdk();
+    const edition = await  sdk.getEdition(process.env.CONTRACT_ADDRESS!)
+    const result = await edition.setApprovalForAll(process.env.PACK_CONTRACT_ADDRESS!, true);
+    res.send(result);
+});
+
 export default router;
