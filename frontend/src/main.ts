297d256f8ab71c84c0f98e54bbab8b09a6b16f49
VladislavMapleLeafs <vladislavvasilenko2001@gmail.com>
2023-11-06 19:07:57 +0200

    rename Decks component to DecksVue

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,34 +1,34 @@
-import './assets/main.css'
-import './assets/cdn.jsdelivr.net_npm_bootstrap@5.3.2_dist_css_bootstrap.min.css'
-import PrimeVue from 'primevue/config'
-import 'primevue/resources/themes/lara-light-purple/theme.css'
-import ProfileInfo from './components/ProfileInfo.vue'
-import AuthorizationPage from './components/AuthorizationPage.vue'
-import MainPage from './components/MainPage.vue'
-import AboutChronos from './components/AboutChronos.vue'
-import Decks from './components/Decks.vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import "./assets/main.css";
+import "./assets/cdn.jsdelivr.net_npm_bootstrap@5.3.2_dist_css_bootstrap.min.css";
+import PrimeVue from "primevue/config";
+import "primevue/resources/themes/lara-light-purple/theme.css";
+import ProfileInfo from "./components/ProfileInfo.vue";
+import AuthorizationPage from "./components/AuthorizationPage.vue";
+import MainPage from "./components/MainPage.vue";
+import AboutChronos from "./components/AboutChronos.vue";
+import DecksVue from "./components/Decks.vue";
+import { createRouter, createWebHistory } from "vue-router";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
+import App from "./App.vue";
 
-const app = createApp(App)
-app.use(PrimeVue)
+const app = createApp(App);
+app.use(PrimeVue);
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', component: MainPage },
-    { path: '/AuthorizationPage', component: AuthorizationPage },
-    { path: '/ProfileInfo', component: ProfileInfo },
-    { path: '/AboutChronos', component: AboutChronos },
-    { path: '/Decks', component: Decks }
-  ]
-})
+    { path: "/", component: MainPage },
+    { path: "/AuthorizationPage", component: AuthorizationPage },
+    { path: "/ProfileInfo", component: ProfileInfo },
+    { path: "/AboutChronos", component: AboutChronos },
+    { path: "/DecksVue", component: DecksVue },
+  ],
+});
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
 
-app.mount('#app')
+app.mount("#app");
