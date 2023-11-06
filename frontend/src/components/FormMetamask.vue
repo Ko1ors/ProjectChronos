def9c51a356677f3645cd3ce62a423749f3fa345
VladislavMapleLeafs <vladislavvasilenko2001@gmail.com>
2023-11-06 18:59:20 +0200

    added page for deck and fixed authorization page

---
+++ frontend/src/components/FormMetamask.vue
@@ -0,0 +1,155 @@
+<script setup lang="ts">
+import Button from 'primevue/button'
+import detectEthereumProvider from '@metamask/detect-provider';
+import MetaMaskOnboarding from '@metamask/onboarding'
+import { ref } from 'vue';
+import { Buffer } from 'buffer';
+import { getAuthMessagesAsync, loginAsync } from '../api/api';
+import type { Response } from '../api/api';
+
+const isMetamaskConnected = ref(false);
+const connecting = ref(false);
+const onboarding = new MetaMaskOnboarding();
+const networkVersion = ref(0);
+const accounts = ref<string[]>([]);
+const sign = ref<string>("");
+let messageResponse: Response<string>;
+let responseInfo: string;
+
+const mumbaiId = 80001;
+
+const handleChainChanged = () => {
+  window.location.reload();
+};
+
+const connectMetamask = async () => {
+  if (connecting.value) return;
+
+  connecting.value = true;
+  const provider = await detectEthereumProvider();
+
+  if (provider) {
+    // Request access to the user's MetaMask account
+    await (provider as any).request({ method: 'eth_requestAccounts' });
+
+    networkVersion.value = await (provider as any).request({ method: 'net_version' });
+
+    // Reload the page on network change
+    provider.on('chainChanged', handleChainChanged);
+
+    accounts.value = await (provider as any).request({ method: 'eth_requestAccounts' }).catch((err: any) => {
+      if (err.code === 4001) {
+        // EIP-1193 userRejectedRequest error
+        // If this happens, the user rejected the connection request.
+        console.error('Please connect to MetaMask.');
+      } else {
+        console.error(err);
+      }
+    });
+
+    // Set the isMetamaskConnected flag to true
+    isMetamaskConnected.value = true;
+  } else {
+    // If the user doesn't have MetaMask installed, display the onboarding screen
+    onboarding.startOnboarding();
+
+  }
+  connecting.value = false;
+  if (!(isMetamaskConnected.value && networkVersion.value != mumbaiId)) signMessageAsync();
+};
+
+const signMessageAsync = async () => {
+  const provider = await detectEthereumProvider();
+  // For historical reasons, you must submit the message to sign in hex-encoded UTF-8.
+  // This uses a Node.js-style buffer shim in the browser.
+  messageResponse = await getAuthMessagesAsync(accounts.value[0]);
+  if (messageResponse.data) {
+    const msg = `0x${Buffer.from(messageResponse.data, 'utf8').toString('hex')}`;
+    sign.value = await (provider as any).request({
+      method: 'personal_sign',
+      params: [msg, accounts.value[0]],
+    });
+    await loginAsync(accounts.value[0], sign.value, true);
+    window.open("/ProfileInfo", "_self");
+  }
+  else {
+    responseInfo = messageResponse.message;
+  }
+}
+
+
+</script>
+
+<template>
+  <div class="col-lg-4 meta-form">
+    <h4 class="wallet-text">Connect with your wallet to access your profile</h4>
+    <Button @click="connectMetamask" class="button-metamask">
+      <img class="fox" src="/src/resources/MetaMask_Fox.svg.png" alt="Metamask" />
+      <span>Login via metamask</span>
+    </Button>
+    <p v-if="isMetamaskConnected && networkVersion != mumbaiId">Please switch to Polygon Mumbai Testnet</p>
+    <p v-if="responseInfo">Error in response: {{ responseInfo }}</p>
+    <div v-if="!responseInfo && isMetamaskConnected && networkVersion === mumbaiId">
+      <router-link to="/ProfileInfo" target="_blank"></router-link>
+    </div>
+
+  </div>
+</template>
+
+<style scoped lang="scss">
+.meta-form {
+  border: 2px solid;
+  height: 300px;
+  display: flex;
+  margin-top: 50px;
+  border-radius: 5px;
+  background-color: var(--surface-section);
+  flex-direction: column;
+  padding: 20px;
+  align-items: center;
+  margin-left: 20px;
+  margin-right: 20px;
+
+  @include media-breakpoint-up(md) {
+    margin-left: 50px;
+    margin-right: 50px;
+  }
+
+  @include media-breakpoint-up(lg) {
+    margin-left: auto;
+    margin-right: auto;
+  }
+}
+
+.p-button {
+  border-radius: 6px;
+}
+
+.button-metamask {
+  width: 50%;
+  height: 20%;
+  margin-top: 80px;
+}
+
+.button-metamask span {
+  font-size: large;
+  font-weight: bold;
+  display: block;
+  text-align: center;
+  width: 100%;
+}
+
+.fox {
+  width: 30px;
+  height: 30px;
+}
+
+.content {
+  background-color: var(--surface-ground);
+  height: 100vh;
+}
+
+.wallet-text {
+  text-align: center;
+}
+</style>
---
+++ frontend/src/components/TemplateVue.vue
@@ -0,0 +1,108 @@
+
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand">Menu</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <router-link class="nav-link" to="/AuthorizationPage">Metamask authorization</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link class="nav-link" to="/ProfileInfo">Profile
+                        <img class="profile" src="../resources/Profile.png" alt="Profile">
+                    </router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link class="nav-link" to="/AboutChronos">About Chronos
+                    </router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link class="nav-link" to="/Decks">Decks
+                    </router-link>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+
+    <div class="info">
+        <slot />
+    </div>
+    <div class="footer">&copy;<span id="year"> 2023</span><span> Chronos. All rights reserved.</span></div>
+</template>
+
+<style scoped lang="scss">
+.navbar-brand {
+    margin-left: 10px;
+    margin-bottom: 3px;
+}
+
+.navbar {
+    background-color: var(--surface-overlay);
+}
+
+.info {
+    padding-bottom: 128px;
+    min-height: calc(100vh - 59px);
+}
+
+.profile {
+    width: 15px;
+    margin-bottom: 2px;
+}
+
+.p-button {
+    border-radius: 6px;
+}
+
+.section-title {
+    margin-top: 50px;
+    height: 150px;
+}
+
+.title {
+    text-align: center;
+    font-family: 'Times New Roman', Times, serif;
+    font-size: 60px;
+}
+
+.nav-item {
+    margin-left: 10px;
+
+    @include media-breakpoint-up(lg) {
+        margin-left: 0px;
+    }
+}
+
+.navbar-toggler {
+    margin-right: 10px;
+}
+</style>
+
+<style>
+body {
+    min-height: 100vh;
+    background-color: var(--surface-ground) !important;
+    position: relative;
+}
+
+.footer {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    padding: 2rem;
+    background-color: var(--surface-card);
+    color: black;
+    font-weight: 500;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 80px;
+}
+</style>
+
