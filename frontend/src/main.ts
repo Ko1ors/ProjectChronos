62aa2e221652a92d0fbdf145beb91bbc24917cc4
VladislavMapleLeafs <vladislavvasilenko2001@gmail.com>
2023-11-06 19:12:54 +0200

    fixed main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,25 +5,7 @@ import 'primevue/resources/themes/lara-light-purple/theme.css'
 import ProfileInfo from './components/ProfileInfo.vue'
 import AuthorizationPage from './components/AuthorizationPage.vue'
 import MainPage from './components/MainPage.vue'
-import AboutChronos from './components/AboutChronos.vue'
-import DecksVue from './components/Decks.vue'
-import { createRouter, createWebHistory } from 'vue-router'
-
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
-import App from './App.vue'
-
-const app = createApp(App)
-app.use(PrimeVue)
-
-import './assets/main.css'
-import './assets/cdn.jsdelivr.net_npm_bootstrap@5.3.2_dist_css_bootstrap.min.css'
-import PrimeVue from 'primevue/config'
-import 'primevue/resources/themes/lara-light-purple/theme.css'
-import ProfileInfo from './components/ProfileInfo.vue'
-import AuthorizationPage from './components/AuthorizationPage.vue'
-import MainPage from './components/MainPage.vue'
+import DecksVue from './components/DecksVue.vue'
 import AboutChronos from './components/AboutChronos.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
@@ -42,14 +24,9 @@ const router = createRouter({
     { path: '/AuthorizationPage', component: AuthorizationPage },
     { path: '/ProfileInfo', component: ProfileInfo },
     { path: '/AboutChronos', component: AboutChronos },
-    { path: '/DecksVue', component: DecksVue }
+    { path: '/Decks', component: DecksVue }
   ]
 })
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
 
 app.use(createPinia())
 app.use(router)
