ee3feaf25323e9ac883fcf2f2ae6e2d8794f38b4
Ko1ors <kolorsnik181200@gmail.com>
2023-11-08 22:56:48 +0200

    Backend: opponents name generator

---
+++ ProjectChronos/Services/NameGenerator.cs
@@ -0,0 +1,29 @@
+﻿namespace ProjectChronos.Services
+{
+    public class NameGenerator
+    {
+        private static Random random = new Random();
+
+        private static string[] adjectives;
+        private static string[] nouns;
+
+        public static void LoadWordLists()
+        {
+            adjectives = File.ReadAllLines("Assets\\adjectives.txt");
+            nouns = File.ReadAllLines("Assets\\nouns.txt");
+        }
+
+        public static string GenerateName()
+        {
+            if (adjectives == null || nouns == null)
+            {
+                LoadWordLists();
+            }
+
+            string adjective = adjectives[random.Next(adjectives.Length)];
+            string noun = nouns[random.Next(nouns.Length)];
+
+            return $"{adjective} {noun}";
+        }
+    }
+}
