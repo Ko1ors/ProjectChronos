8b0ca7f6c62f088c55032654771e25054c7350df
Ko1ors <kolorsnik181200@gmail.com>
2023-11-24 17:40:18 +0200

    Backend: added IsEvaded column to attack turn

--- ProjectChronos.Common/Interfaces/Entities/IMatchAttackTurn.cs
+++ ProjectChronos.Common/Interfaces/Entities/IMatchAttackTurn.cs
@@ -6,6 +6,8 @@
 
         public int TargetCardId { get; set; }
 
+        public bool IsEvaded { get; set; }
+
         public IMatchDrawnCard AttackCard { get; set; }
 
         public IMatchDrawnCard TargetCard { get; set; }
