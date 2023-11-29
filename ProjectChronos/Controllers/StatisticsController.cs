93c932da68ecb1608d33355af067dfad18626fa2
Ko1ors <kolorsnik181200@gmail.com>
2023-11-29 16:49:53 +0200

    Backend: StatisticsController

---
+++ ProjectChronos/Controllers/StatisticsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ProjectChronos.Common.Interfaces.Services;
+using ProjectChronos.Entities;
+
+namespace ProjectChronos.Controllers
+{
+    public class StatisticsController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly IUserStatsService _userStatsService;
+
+        public StatisticsController(UserManager<User> userManager, IUserStatsService userStatsService)
+        {
+            _userManager = userManager;
+            _userStatsService = userStatsService;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> GetUserStatistics()
+        {
+            var currentUser = await _userManager.FindByNameAsync(User.Identity!.Name);
+            var stats = await _userStatsService.GetCompositeUserStatsAsync(currentUser);
+            return Ok(stats);
+        }
+    }
+}
